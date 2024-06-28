57cd0d77bc26a2faadd2ba7c9c7fdf8b88f18e3c
Andrea Paruta <159964313+AndreaParuta@users.noreply.github.com>
2024-06-28 15:05:40 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,10 +1,3 @@
-function skillsmember() {
-    var member = {
-        name: "John Doe",
-        age: 26,
-        skills: ["JavaScript", "React", "Node", "MongoDB"],
-    };
-    return member;
-}
-module.exports = skillsmember;
-
+function skillsMember() {
+    return {
+      
